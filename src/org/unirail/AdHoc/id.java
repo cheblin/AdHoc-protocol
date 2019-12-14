cb7b12222e3a092da358cc5e8c051aa75003209d
Chikirev Sirguy <info@unirail.org>
2019-12-14 09:23:11 +0800

    add RetentionPolicy

--- src/org/unirail/AdHoc/id.java
+++ src/org/unirail/AdHoc/id.java
@@ -1,9 +1,12 @@
 package org.unirail.AdHoc;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 //Pack unique identification. Applied by the code generator.
+@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface id {
 	long value();
